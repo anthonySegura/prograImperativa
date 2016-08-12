8a0ac10e0483a104d0c9f00e5af64af8c3b7a059
anthonySegura <ansechaves@hotmail.com>
2016-08-12 15:47:08 -0600

    Algunos cambios

--- PrograImperativa/Joystick.cpp
+++ PrograImperativa/Joystick.cpp
@@ -2,30 +2,35 @@
 #include <stdlib.h>
 #include <GL\glut.h>
 
-int UNO = 0x01;
+int UNO = 1;
 int DOS = 2;
-int TRES = 3;
-int CUATRO = 4;
-int ARRIBA = 5;
-int ABAJO = 6;
-int IZQUIERDA = 7;
-int DERECHA = 8;
+int TRES = 4;
+int CUATRO = 8;
 double axisX = 0.0, axisY = 0.0, axisZ = -4.5;
+//Ejes de la esfera
 GLfloat xRotated = 0, yRotated = 0, zRotated = 0;
+float bordeIzq = -2.330000;
+float bordeDer =  2.330000;
+float bordeSup = -1.100000;
+float bordeInf =  1.100000;
 
 void joystick(unsigned int buttonmask, int x, int y, int z) {
 	int *px = &x;
-	if (buttonmask & 0x1) {
-		printf("Button 1 pressed\n");
+	if (buttonmask & UNO) {
 		yRotated += 1;
-		axisX -= 0.01;
+		if(axisX > bordeIzq) axisX -= 0.01;
 	}
-	else if (buttonmask & 0x4) {
+	else if (buttonmask & TRES) {
 		yRotated -= 1;
-		axisX += 0.01;
-		
+		if(axisX < bordeDer) axisX += 0.01;
 	}
-	else if (buttonmask & 0x2) {
-		
+	else if (buttonmask & DOS) {
+		xRotated -= 1;
+		if(axisY > bordeSup) axisY -= 0.01;
 	}
+	else if (buttonmask & CUATRO) {
+		xRotated += 1;
+		if(axisY < bordeInf)axisY += 0.01;
+	}
+	//printf("boton %d\n" , buttonmask);
 }
