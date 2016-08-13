21d69608d8572ca907b6b748c6f688ba6c5ac906
anthonySegura <ansechaves@hotmail.com>
2016-08-12 20:25:18 -0600

    Funciones de movimientos de la esfera

--- PrograImperativa/Joystick.cpp
+++ PrograImperativa/Joystick.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL\glut.h>
+#include <math.h>
 
 //Codigos de los botones del Joystick
 int UNO = 1;
@@ -21,8 +22,63 @@ float bordeDer =  2.330000;
 float bordeSup = -1.100000;
 float bordeInf =  1.100000;
 
+GLdouble radio = 0.5;
+
+bool compare_float(float f1, float f2)
+{
+	return fabs(f1 - f2) < 1.0 / 10000000;
+}
+
+
+void mover(float x, float y) {
+
+	while (!(compare_float(axisX , x)) || !(compare_float(axisY , y))) {
+		if (axisX < x) {
+			yRotated -= 0.2;
+			axisX += 0.001;
+		}
+		else {
+			yRotated += 0.2;
+			axisX -= 0.001;
+		}
+		if (axisY < y) {
+			xRotated += 0.2;
+			axisY += 0.001;
+		}
+		else {
+			xRotated -= 0.2;
+			axisY -= 0.001;
+		}
+		printf("Eje x : %f Eje y %f\n", axisX, axisY);
+		glMatrixMode(GL_MODELVIEW);
+		// clear the drawing buffer.
+		glClear(GL_COLOR_BUFFER_BIT);
+		// clear the identity matrix.
+		glLoadIdentity();
+		// traslate the draw by z = -4.0
+		// Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
+		glTranslatef(axisX, axisY, axisZ);
+		// Green color used to draw.
+		glColor3f(0.0, 1.0, 0.0);
+		// changing in transformation matrix.
+		// rotation about X axis
+		glRotatef(xRotated, 1.0, 0.0, 0.0);
+		// rotation about Y axis
+		glRotatef(yRotated, 0.0, 1.0, 0.0);
+		// rotation about Z axis
+		glRotatef(zRotated, 0.0, 0.0, 1.0);
+		// scaling transfomation 
+		glScalef(1.0, 1.0, 1.0);
+		// built-in (glut library) function , draw you a sphere.
+		glutSolidSphere(radio, 20, 20);
+		// Flush buffers to screen
+
+		glFlush();
+	}
+}
+
 void joystick(unsigned int buttonmask, int x, int y, int z) {
-	int *px = &x;
+
 	if (buttonmask & UNO) {
 		yRotated += 1;
 		if(axisX > bordeIzq) axisX -= 0.01;
@@ -39,9 +95,14 @@ void joystick(unsigned int buttonmask, int x, int y, int z) {
 		xRotated += 1;
 		if(axisY < bordeInf)axisY += 0.01;
 	}
-
+	else if (buttonmask & ANIMACION) {
+		mover(0.000000, 0.000000);
+	}
+	
 	else if (buttonmask & SALIR) {
 		glutDestroyWindow(WINDOW_ID);
 	}
-	printf("boton %d\n" , buttonmask);
+
+	printf("Eje x : %f Eje y : %f\n", axisX, axisY);
+	
 }
